f7614fb23d7c3788505006afa9637e60d2380620
Kapil Dadhich <kapildadhich075@gmail.com>
2025-02-22 20:59:02 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,11 @@ import subRoutes from "./routes/sub.routes.js"; // Updated to match the actual f
 
 const app = express();
 
+const allowedOrigins = [
+  "https://mp4titles.netlify.app/",
+  "http://localhost:5173",
+];
+
 configDotenv();
 const PORT = process.env.PORT || 3000;
 export const NODE_ENV = process.env.NODE_ENV || "development";
@@ -15,13 +20,19 @@ app.use(fileUpload());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://mp4titles.netlify.app/", "http://localhost:5173"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
 
 app.use("/api/subs", subRoutes);
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log("server started");
 });
